af66afe5a112e6caf8ad61d2d6ac3970836c3a0e
StanHustler <cf260469080@sina.com>
2021-01-23 13:42:02 +0800

    FEATURE| Now script can download the songs with name

--- get.py
+++ get.py
@@ -3,14 +3,18 @@ import json
 
 
 _id_ = []
+_name_ = []
 _RGBurl_ = "http://musicapi.leanapp.cn/playlist/detail?id=3778678"  # 热歌榜
-_BaseUrl_ = "http://music.163.com/song/media/outer/url?id="  # 解析接口
+_DownBaseUrl_ = "http://music.163.com/song/media/outer/url?id="  # 解析接口
+_SrhBaseUrl_ = "http://musicapi.leanapp.cn/search?keywords="
 
-for i in range(0, 1):  # 获取榜单内歌曲id
+for i in range(0, 2):  # 获取榜单内歌曲id，搜索id对应名称
     _id_.append(str(json.loads(requests.get(_RGBurl_).text)["playlist"]["trackIds"][i]["id"]))
+    _name_.append(str(json.loads(requests.get(_SrhBaseUrl_ + _id_[i]).text)["result"]["songs"][0]["name"]))
 
-for j in range(len(_id_)):
-    _url_ = _BaseUrl_ + str(_id_[j]) + ".mp3"
+for j in range(0, len(_id_)):
+    _url_ = _DownBaseUrl_ + str(_id_[j]) + ".mp3"
     r = requests.get(_url_)
-    with open(str(_id_[j]) + ".mp3", 'wb') as f:
+    with open(str(_name_[j]) + ".mp3", 'wb') as f:
         f.write(r.content)
+        print(str(j)+"done")
